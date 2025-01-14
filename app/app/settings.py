15ff15f9bb6560892f13ad8c6bfb2670d8bf2247
Lin Lập Kiến <linlapkien@gmail.com>
2025-01-14 10:37:33 +0700

    added vercel.json file

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d-d))i3hf^u!2ohjy)77um2%du0^+%b)d0=lj*$!7@)w%+e6^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '.vercel.app',]
 
 
 # Application definition
